b8137b119813b297c04907d65b289ed084f87119
kickhad <adam@adamkickham.com>
2020-03-13 18:02:57 -0400

    a

--- flashfoo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@echo Avrdude
-@echo atmega1284p
-mode
-@echo TYPE COM? for port number
-set /p portno = %id%
-@echo avrdude -v -pm2560 -Cavrdude.conf -cwiring -P%id% -b115200 -D -Uflash:w:firmware.hex:i
